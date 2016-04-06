96cdb2d9d76311110bbc02f6b633a9c20087549f
Nacho Martinez <imartinez@sweatworks.net>
2016-04-06 14:01:41 +0200

    Hover with color and country name

--- index.php
+++ index.php
@@ -5,17 +5,39 @@
 		<script src="js/gdp-data.js"></script>
 		<script src="js/jquery-jvectormap-world-mill-en.js"></script>
 		<link rel="stylesheet" media="all" href="css/jquery-jvectormap-2.0.3.css" />
+		<script>
+			
+		    $(document).ready(function () {
+    			$('#world-map').vectorMap(wrld);
+    			
+    			var mapObj = $('#world-map').vectorMap('get', 'mapObject');
+
+			    $('#countries').on('mouseover mouseout', 'a:first-child', function (event) {
+			        // event.preventDefault();
+			        var elem = event.target,
+			            evtype = event.type,
+			            cntrycode = findRegion(mapObj.regions, $(elem).text());
+			
+			        if (evtype === 'mouseover') {
+			            mapObj.regions[cntrycode].element.setHovered(true);
+			        } else {
+			            mapObj.regions[cntrycode].element.setHovered(false);
+			        };       
+			    });
+    		});
+    		
+			var wrld = {
+			  map: 'world_mill_en',
+			  regionStyle: {
+			    hover: {
+			        "fill": 'red'
+			    }
+			  }
+			};
+		</script>
 	</head>
 	<body>
-		    <div id="world-map-gdp" style="width: 720px; height: 400px"></div>
-		    <script>
-		      //@code_start
-		      $(function(){
-		        $('#world-map-gdp').vectorMap();
-		      });
-		      //@code_end
-		    </script>
-		  
+		    <div id="world-map" style="width: 720px; height: 400px"></div>
 	</body>
 </html>
 
